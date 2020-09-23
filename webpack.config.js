4642516bde5414974611c2260d715cab2d09e199
James Scolamieri <james.scolamieri@gmail.com>
2020-09-23 16:43:38 -0700

    setup webpack, express server, static html, react, package scripting, and most dependencies. setting up testing suite next.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        // ignore transpiling JavaScript from node_modules as it should be that state
+        exclude: /node_modules/,
+        // use the babel-loader for transpiling JavaScript to a suitable format
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+        }
+      }
+    ]
+  },
+};
+
+
