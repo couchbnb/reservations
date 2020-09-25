0ef483b2fa83a4ea408e09e1de3fc98b198b52c1
James Scolamieri <james.scolamieri@gmail.com>
2020-09-25 13:28:30 -0700

    changed app to class component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,21 @@ import PriceSummary from './PriceSummary.jsx';
 import Fees from './Fees.jsx';
 import Reserve from './Reserve.jsx';
 
-const App = (props) => (
-  <div className="buff">
-    <h1 className="App">Hello, couchBNB!</h1>
-    <PriceSummary />
-    <Fees />
-    <Reserve />
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <div className="buff">
+        <h1 className="App">Hello, couchBNB!</h1>
+        <PriceSummary />
+        <Fees />
+        <Reserve />
+      </div>
+    )
+  }
+}
 
 export default App;
