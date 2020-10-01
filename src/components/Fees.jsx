226f3a5a153a4926d439fa1e0c7635256a78f354
James Scolamieri <james.scolamieri@gmail.com>
2020-10-01 16:16:33 -0700

    updated the fee styles to exact match

--- src/components/Fees.jsx
+++ src/components/Fees.jsx
@@ -9,8 +9,29 @@ let Wrapper = styled.div`
   line-height: 20px !important;
 `;
 
+let Message = styled.ul`
+  color: rgb(34, 34, 34) !important;
+  font-family: Circular, -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif !important;
+
+  font-size: 14px !important;
+  line-height: 18px !important;
+  align-items: center !important;
+  display: flex !important;
+  flex-direction: column !important;
+  list-style-type: none !important;
+  margin: 8px 0px 0px !important;
+  padding: 0px !important;
+  text-align: center !important;
+`;
+
+let MessageLi = styled.li`
+  margin-top: 8px !important;
+  white-space: normal !important;
+  font-weight: 325 !important;
+`;
+
 let List = styled.ul`
-  margin: 0px !important;
+  margin: 24px 0px 0px 0px !important;
   padding: 0px !important;
   list-style-type: none !important;
 `;
@@ -54,9 +75,36 @@ let Amount = styled.span`
   white-space: nowrap !important;
 `;
 
+let Total = styled.ul`
+  border-top: 1px solid rgb(221, 221, 221) !important;
+  list-style-type: none !important;
+  margin: 16px 0px 0px !important;
+  padding: 24px 0px 12px !important;
+`;
+
+let TotalLi = styled.li`
+  display: flex !important;
+  padding-bottom: 0px !important;
+`;
+
+let TotalLabel = styled.span`
+  flex-shrink: 1 !important;
+  flex-grow: 1 !important;
+  white-space: normal !important;
+  font-weight: 650 !important;
+`;
+
+let TotalAmt = styled.span`
+  padding-left: 16px !important;
+  white-space: nowrap !important;
+  font-weight: 650 !important;
+`;
+
 const Fees = (props) => (
   <Wrapper>
-    <div>you wont be charged yet</div>
+    <Message>
+      <MessageLi>Your reservation will save</MessageLi>
+    </Message>
     <List>
       <Row>
         <LineItem>
@@ -84,7 +132,12 @@ const Fees = (props) => (
           <Amount>$300</Amount>
         </LineItem>
       </Row>
-      <li>total</li>
+      <Total>
+        <TotalLi>
+          <TotalLabel>Total</TotalLabel>
+          <TotalAmt>$1000</TotalAmt>
+        </TotalLi>
+      </Total>
     </List>
   </Wrapper>
 );
