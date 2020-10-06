0e7654bf6b520849c21c6a682e4f69ceeb8f8b65
James Scolamieri <james.scolamieri@gmail.com>
2020-10-06 14:32:37 -0700

    added instructions to README and changed port to 3005

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3005;
 // const DB = require('./database/index.js');
 const control = require('./database/control.js');
 const parser = require('body-parser')
